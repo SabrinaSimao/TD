1d67b8dc9a8935f6d55f7db4e894af4750adf093
chends888 <lchends@gmail.com>
2016-04-29 17:02:58 -0300

    Adicionando um arquivo separado para monstros e slime e removendo monstro e slime do outro arquivo

--- Classe_Monstro_Slime_Wave_Spawn.py
+++ Classe_Monstro_Slime_Wave_Spawn.py
@@ -14,11 +14,3 @@ class Slime (Monster):
     speed = 60
     def __init__ (self, max_hp):
         self.current_hp = max_hp
-
-class Spawn:
-    def __init__ (self, s_frequency):
-        self.s_frequency = s_frequency
-
-class Wave:
-    def __init__ (self, w_frequency):
-        self.w_frequency = w_frequency
---
+++ monstro_slime.py
@@ -0,0 +1,7 @@
+class Spawn:
+    def __init__ (self, s_frequency):
+        self.s_frequency = s_frequency
+
+class Wave:
+    def __init__ (self, w_frequency):
+        self.w_frequency = w_frequency
