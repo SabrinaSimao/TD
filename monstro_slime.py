e92463cd8d7f2dbec2b10a979d0b49924a56d2a9
Yiaannn <Yiaannn@gmail.com>
2016-04-29 21:19:02 +0000

    Classes Spawn e Wave
    Bastante trabalo realizado principalmente nas classes Spawn e Wave, acho
    que podem ser consideradas prontas por enquanto

--- monstro_slime.py
+++ monstro_slime.py
@@ -1,16 +1,23 @@
 class Monster:
-    def __init__ (self, max_hp, speed):
-        self.max_hp = max_hp
-        self.current_hp = max_hp
-        self.speed = speed
-        
-    def dead (current_hp):
-        if current_hp == 0:
-            return 'dead'
+    cycle= 60
+    def __init__ (self, mapa):
+        self.mapa=mapa
+    
+    def main(self):
+        #função que executa o monstro, chamada por CycleManager
+        self.move()
+        if self.current_hp == 0:
+            self.mapa.erase(self)
+            
+    #def move(self):
+    #TODO: função move()    
 
 class Slime (Monster):
     icon = "slime.bmp"
     max_hp = 10
-    speed = 60
-    def __init__ (self, max_hp):
-        self.current_hp = max_hp
+    cycle = 60
+    
+    def __init__ (self, mapa):
+        self.current_hp= self.max_hp
+        self.mapa= mapa
+        
--- spawn_wave.py
+++ spawn_wave.py
@@ -1,7 +1,52 @@
 class Spawn:
-    def __init__ (self, s_frequency):
-        self.s_frequency = s_frequency
-
+    ##
+    #Overview:
+    #
+    ##
+    #Atributos:
+    #
+    #   - wave_wait - intervalo de criação entre o fim de uma wave e a próxima, em ciclos 
+    #   - self.cycle - intervalo em ciclos até a próima execução de main()
+    #   - mapa - Referência ao objeto Mapa, para pedir para ele criar os monstros
+    
+    def __init__ (self, mapa):
+        
+        self.wave_wait= 600
+        self.cycle= self.wave_wait
+        self.current_wave= None
+        self.mapa= mapa
+        
+        
+        
+        ##Criar as waves e guardar em uma lista
+        self.wave_list=[]
+        
+        self.wave_list.append( Wave(180, ["Slime", "Slime", "Slime"]) )
+        self.wave_list.append( Wave(120, ["Slime","Slime", "Slime", "Slime", "Slime", "Slime", "Slime", "Slime"]) )
+        ##
+        
+    def main(self):
+        ## Se não tiver Wave ativa, carregar a próxima
+        if self.current_wave == None:
+            self.current_wave= (self.wave_list[0])
+            self.wave_list.pop(0)
+        ##
+            
+        ## Pedir pro mapa criar o monstro, se não conseguir criar (retorno -1)  tentar denovo próximo ciclo
+        if self.mapa.create(self.current_wave.monster_list[0], "Spawn") != -1:
+            
+            self.current_wave.monster_list.pop(0)
+            self.cycle= self.current_wave.monster_interval
+        else:
+            
+            self.cycle= 1
+        ##
+            
+        if len( self.currentwave.monster_list ) == 0:
+            self.current_wave= None
+            self.cycle= self.wave_wait
+    #
 class Wave:
-    def __init__ (self, w_frequency):
-        self.w_frequency = w_frequency
+    def __init__ (self,monster_interval, monster_list):
+        self.monster_interval= monster_interval #intervalo de espera entre criar os monstros, em ciclos
+        self.monster_list= monster_list #lista dos TIPOS de monstro a serem gerados
