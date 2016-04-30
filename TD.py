580597312d95f32cec00cd0dad59cf3a3884b9b7
SabrinaSimao <sabrina_simao@hotmail.com>
2016-04-29 23:52:25 -0300

    adicionei o mapa tiled

--- TD.py
+++ TD.py
@@ -7,3 +7,15 @@ Created on Wed Apr 27 10:27:41 2016
 
 """Projeto final de DesSoft da Engenharia 1C
     Tower Defense"""
+    
+class Tower():
+    def __init__(self, alcance, reload, dano, imagem, nome):
+        self.alcance = alcance
+        self.reload = reload
+        self.dano = dano
+        self.imagem = imagem
+        self.nome = nome
+
+
+canhao = Tower(5, 5, 5, canhao_image, "canhao")
+
--- Template.py
+++ Template.py
@@ -23,10 +23,12 @@ RED = (255, 0, 0)
 pygame.init()
  
 # Set the width and height of the screen [width, height]
-size = (800, 600)
+screen_w = 800
+screen_h = 600
+screen_size = (screen_w, screen_h) #se for apenas 600, nao vai caber uma tile de 32x32 no fim da tela
 screen = pygame.display.set_mode(size)
  
-pygame.display.set_caption("TD")
+pygame.display.set_caption("Tower Defense")
  
 # Loop until the user clicks the close button.
 done = False
@@ -38,7 +40,7 @@ clock = pygame.time.Clock()
 slime_image = pygame.image.load("slime.png").convert()
 slime_dmg_image = pygame.image.load("slime_dmg.png").convert()
 canhao_image = pygame.image.load("canhao.png").convert()
-
+fundo = pygame.image.load("teste.png").convert()
 
 
 
@@ -58,10 +60,10 @@ while not done:
  
     # If you want a background image, replace this clear with blit'ing the
     # background image.
-    screen.fill(WHITE)
+    screen.blit(fundo,(0,0))
     for x in range(0, 11):
         for y in range(0, 11):
-            screen.blit(slime_image, [x*32,y*32])
+            screen.blit(slime_dmg_image, [x*32,y*32])
     # --- Drawing code should go here
  
     # --- Go ahead and update the screen with what we've drawn.
