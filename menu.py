e459576ffdd2cdd992f45c98822392cfecf45513
chends888 <lchends@gmail.com>
2016-05-12 22:33:19 -0300

    Adicionando "funções" aos botões
    Adicionei um valor arbitriário de retorno quando o único botão que temos
    é clicado, variável click, com a função do pygame de capturar quando o
    mouse é clicado

--- menu.py
+++ menu.py
@@ -9,8 +9,15 @@ import pygame
 
 #mouse_position = pygame.mouse.get_pos ()
 
-def cannon_button (mouse_position, screen, x, y, width, height, color1, color2):
+def cannon_button (mouse_position, screen, x, y, width, height, color1, color2, click):
+    #Coloração do botão
     if x + width > mouse_position [0] > x and y + height > mouse_position [1] > y:
         pygame.draw.rect (screen, color1, (x, y, width, height))
+        #Funções do botão
+        if click [0] == 1:
+            print ('1') #Algo para construir canhão
     else:
         pygame.draw.rect (screen, color2, (x, y, width, height))
+        
+
+    
