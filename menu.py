ec72eab5c5c8ab27cd4205d924ffe8199c8a49c5
Formulos <paulotozp@gmail.com>
2016-05-18 10:55:36 -0300

    More buttons
    que não fazem nada,e dão erro

--- menu.py
+++ menu.py
@@ -9,11 +9,23 @@ import pygame
 
 #mouse_position = pygame.mouse.get_pos ()
 
-def cannon_button (screen, x, y, width, height, color,):
+def cannon_button (screen):
     #Coloração do botão
-    
 
-    pygame.draw.rect (screen, color, (x, y, width, height))
+#    pygame.draw.rect (screen, color, (x, y, width, height))
+    pygame.draw.rect (screen, (0, 255, 0), (900, 100, 32, 32,)) # cannon_button
+    pygame.draw.rect (screen, (255, 0, 0), (900, 200, 32, 32,)) # ballista_button
+    pygame.draw.rect (screen, (255, 0, 0), (900, 300, 32, 32,)) # tower 3
+    
+    return 'Cannon'
         
+def ballista_button (screen):
+    
+    #    pygame.draw.rect (screen, color, (x, y, width, height))
+    pygame.draw.rect (screen, (255, 0, 0), (900, 100, 32, 32,)) # cannon_button
+    pygame.draw.rect (screen, (0, 255, 0), (900, 200, 32, 32,)) # ballista_button
+    pygame.draw.rect (screen, (255, 0, 0), (900, 300, 32, 32,)) # tower 3
+    
+    return 'ballista'
 
  
