261ca511a93f13850e49b838ef5dee085037c6e7
Yiaannn <Yiaannn@gmail.com>
2016-05-20 00:09:15 +0000

    Bugfix na AI
    agora ele evalua corretamente o valor de movimento nas casas das bordas
    do mapa

--- gamemap.py
+++ gamemap.py
@@ -95,17 +95,17 @@ class GameMap:
             keep_on= False
             for i in range(len(grid)):
                 for j in range(len(grid[i])):
-                    if grid[i][j].move_value == level and i != 0 and i != len(grid)-1 and j!= 0 and j != len(grid[i])-1:
-                        if grid[i][j+1].move_value == -1:
+                    if grid[i][j].move_value == level:
+                        if j != len(grid[i])-1 and grid[i][j+1].move_value == -1:
                             grid[i][j+1].move_value= level+1
                             keep_on= True
-                        if grid[i][j-1].move_value == -1:
+                        if j!= 0 and grid[i][j-1].move_value == -1:
                             grid[i][j-1].move_value= level+1
                             keep_on= True
-                        if grid[i+1][j].move_value == -1:
+                        if i != len(grid)-1 and grid[i+1][j].move_value == -1:
                             grid[i+1][j].move_value= level+1
                             keep_on= True
-                        if grid[i-1][j].move_value == -1:
+                        if i != 0 and grid[i-1][j].move_value == -1:
                             grid[i-1][j].move_value= level+1
                             keep_on= True
             level+=1
